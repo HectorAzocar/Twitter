a1aa7e723ba3d48eec485d581195c78c6cbccf5c
hectorazocar <hectorj.azocar@gmailcom>
2021-02-24 14:42:49 -0300

    install gem activeadmin

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,20 +38,6 @@ class TweetsController < ApplicationController
     end
   end
   
-  # def edit
-  # end
-
-  # def update
-  #     respond_to do |format|
-  #         if @tweet.update(tweet_params)
-  #           format.html { redirect_to @tweet, notice: "el tweet fue modificado" }
-  #           format.json { render :show, status: :ok, location: @tweet }
-  #         else
-  #           format.html { render :edit, status: :unprocessable_entity }
-  #           format.json { render json: @tweet.errors, status: :unprocessable_entity }
-  #         end
-  #       end
-  # end
 
   def destroy
       @tweet = Tweet.find(params[:id])
@@ -66,15 +52,11 @@ class TweetsController < ApplicationController
   
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+  
   def set_tweet
     @tweet = Tweet.find(params[:id])
   end
-
-  # Only allow a list of trusted parameters through.
-  # def retweet_params
-  #   params.require(:retweet).permit(:retweet_id, :content).merge(user_id: current_user.id)
-  # end
+  
   def tweet_params
     params.require(:tweet).permit(:contents, :id_user, :tweet_id)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,8 @@ class User < ApplicationRecord
         
   has_many :tweets
   has_many :likes
-  
-  def tweets_for_me(friends)
-    filtro = Hash.new
-    friends.each do |f|
-      if current_user.id = f.friends_id 
-        filtro.push(f.friend_id)
-      end
-      return filtro
-    end
+    
 end
 
-end
 
 
